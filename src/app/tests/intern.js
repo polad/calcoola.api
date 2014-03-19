50c8963d79d71cdbc5610cc48920ebfbc630f801
Polad Mirzayev <polad.mirzayev@gmail.com>
2014-03-19 06:25:42 +0000

    Added SauceConnect settings to intern

--- src/app/tests/intern.js
+++ src/app/tests/intern.js
@@ -1,6 +1,19 @@
 define({
+    proxyPort: 9009,
+    proxyUrl: "http://localhost:9009/",
+    capabilities: {
+        "selenium-version": "2.37.0"
+    },
+    environments: [
+    ],
     maxConcurrency: 3,
-    useSauceConnect: false,
+    useSauceConnect: true,
+    webdriver: {
+        host: "localhost",
+        port: 4444,
+        username: process.env.SAUCE_USERNAME,
+        accessKey: process.env.SAUCE_ACCESS_KEY
+    },
     loader: {
         baseUrl: process.env.BASE_DIR+"/src/",
         packages: [
