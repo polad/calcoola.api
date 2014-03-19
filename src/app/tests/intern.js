53a749dc71a7714b1601fdc1adabdb6c0d71a8d3
Polad Mirzayev <polad.mirzayev@gmail.com>
2014-03-19 06:07:21 +0000

    Removed Selenium settings and disabled SauceConnect from intern

--- src/app/tests/intern.js
+++ src/app/tests/intern.js
@@ -1,20 +1,6 @@
 define({
-    proxyPort: 9000,
-    proxyUrl: "http://localhost:9000/",
-    capabilities: {
-        "selenium-version": "2.37.0"
-    },
-    environments: [
-        { browserName: "chrome", version: "", platform: "Linux" }
-    ],
     maxConcurrency: 3,
-    useSauceConnect: true,
-    webdriver: {
-        host: "localhost",
-        port: 4444,
-        username: process.env.SAUCE_USERNAME,
-        accessKey: process.env.SAUCE_ACCESS_KEY
-    },
+    useSauceConnect: false,
     loader: {
         baseUrl: process.env.BASE_DIR+"/src/",
         packages: [
