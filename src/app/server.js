96eaa3ce0bbe87e7b42ca17f8ccd9b5c77bd0315
Polad Mirzayev <polad.mirzayev@gmail.com>
2014-04-11 18:38:10 +0000

    Moved Loader from under controllers to app

--- src/app/server.js
+++ src/app/server.js
@@ -1,11 +1,11 @@
 require([
     "dojo/node!express",
-    "app/controllers/Loader",
+    "app/Loader",
     "app/config"
-], function(express, ControllersLoader, config) {
+], function(express, AppLoader, config) {
     var app = express();
     
-    var loader = new ControllersLoader({ app: app });
+    var loader = new AppLoader({ app: app });
     
     app.listen(config.appListenPort);
     console.log('Listening on port 3000');
