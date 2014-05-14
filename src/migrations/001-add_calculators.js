a1f78239fa31be193a4cd9ddadc042f14be5ded5
Polad Mirzayev <polad.mirzayev@gmail.com>
2014-05-14 07:32:10 +0000

    Simplified DB migration scripts API

--- src/migrations/001-add_calculators.js
+++ src/migrations/001-add_calculators.js
@@ -1,282 +1,30 @@
 var db = require("./db");
+var data = require("./data/calculators");
 
 exports.up = function(next) {
 
-    db.connectToDB().
-        then(db.createTable("calculators")).
-        then(db.createIndex("name")).
-        then(db.insert(calculators)).
-        then(function() { next(); });
-    
+    db.run("Creating table 'calculators'...", function(r) {
+        return r.tableCreate("calculators");
+    }).then(function(prevResult) {
+        return db.run("Creating index 'name' on table 'calculators'...", function(r) {
+            return r.table("calculators").indexCreate("name");
+        });
+    }).then(function(prevResult) {
+        return db.run("Inserting data into 'calculators' table...", function(r) {
+            return r.table("calculators").insert(data.calculators);
+        });
+    }).then(function() {
+        next();
+    });
+
 };
 
 exports.down = function(next) {
-    
-    db.connectToDB().
-        then(db.dropTable("calculators")).
-        then(function() { next(); });
-    
-};
 
-var calculators = [{
-    name: "Pressure gradient using mud weight, ppg",
-    categories: [ "Basic Formulas" ],
-    args: {
-        a: {
-            label: "Mud Weight",
-            uom: "ppg",
-            defaultValue: 0.0
-        }
-    },
-    uom: "psi/ft",
-    formula: function(args) {
-        return args.a * 0.052;
-    },
-    description: "formula: psi/ft = mud weight, ppg x 0.052"
-}, {
-    name: "Pressure gradient using mud weight, lb/ft&sup3",
-    categories: [ "Basic Formulas" ],
-    args: {
-        a: {
-            label: "Mud Weight",
-            uom: "lb/ft&sup3",
-            defaultValue: 0.0
-        }
-    },
-    uom: "psi/ft",
-    formula: function(args) {
-        return args.a * 0.006944;
-    },
-    description: "formula: psi/ft = mud weight, lb/ft&sup3 x 0.006944"
-}, {
-    name: "Hydrostatic pressure using ppg and feet as the units of measure",
-    categories: [ "Basic Formulas" ],
-    args: {
-        a: {
-            label: "Mud Weight",
-            uom: "ppg",
-            defaultValue: 0.0
-        },
-        b: {
-            label: "True Vertical Depth",
-            uom: "ft",
-            defaultValue: 0
-        }
-    },
-    uom: "psi",
-    formula: function(args) {
-        return args.a * 0.052 * args.b;
-    },
-    description: "formula: psi = mud weight, ppg x 0.052 x true vertical depth (TVD), ft"
-}, {
-    name: "Hydrostatic pressure, psi, using pressure gradient, psi/ft",
-    categories: [ "Basic Formulas" ],
-    args: {
-        a: {
-            label: "Pressure Gradient",
-            uom: "psi/ft",
-            defaultValue: 0.0
-        },
-        b: {
-            label: "True Vertical Depth",
-            uom: "ft",
-            defaultValue: 0
-        }
-    },
-    uom: "psi",
-    formula: function(args) {
-        return args.a * args.b;
-    },
-    description: "formula: psi = pressure gradient, psi/ft x true vertical depth, ft"
-}, {
-    name: "Hydrostatic pressure, psi, using mud weight, lb/ft&sup3",
-    categories: [ "Basic Formulas" ],
-    args: {
-        a: {
-            label: "Mud Weight",
-            uom: "lb/ft&sup3",
-            defaultValue: 0
-        },
-        b: {
-            label: "True Vertical Depth",
-            uom: "ft",
-            defaultValue: 0
-        }
-    },
-    uom: "psi",
-    formula: function(args) {
-        return args.a * 0.006944 * args.b;
-    },
-    description: "formula: psi = mud weight, lb/ft&sup3 x 0.006944 x TVD, ft"
-}, {
-    name: "Hydrostatic pressure, psi, using meters as unit of depth",
-    categories: [ "Basic Formulas" ],
-    args: {
-        a: {
-            label: "Mud Weight",
-            uom: "ppg",
-            defaultValue: 0.0
-        },
-        b: {
-            label: "True Vertical Depth",
-            uom: "m",
-            defaultValue: 0
-        }
-    },
-    uom: "psi",
-    formula: function(args) {
-        return args.a * 0.052 * args.b * 3.281;
-    },
-    description: "formula: psi = mud weight, ppg x 0.052 x TVD, m x 3.281"
-}, {
-    name: "Equivalent Circulating Density (ECD)",
-    categories: [ "Basic Formulas" ],
-    args: {
-        a: {
-            label: "Annular Pressure Loss",
-            uom: "psi",
-            defaultValue: 0
-        },
-        b: {
-            label: "True Vertical Depth",
-            uom: "ft",
-            defaultValue: 0
-        },
-        c: {
-            label: "Mud Weight",
-            uom: "ppg",
-            defaultValue: 0.0
-        }
-    },
-    uom: "ppg",
-    formula: function(args) {
-        return (args.a / 0.052 / args.b) + args.c;
-    },
-    description: "formula: ppg = ((annular pressure loss, psi ) / 0.052 / TVD, ft) + (mud weight in use, ppg)"
-}, {
-    name: "Maximum Allowable Mud Weight from Leak-off Test Data",
-    categories: [ "Basic Formulas" ],
-    args: {
-        a: {
-            label: "Leak-Off Test Pressure",
-            uom: "psi",
-            defaultValue: 0
-        },
-        b: {
-            label: "Casing Shoe TVD",
-            uom: "ft",
-            defaultValue: 0
-        },
-        c: {
-            label: "Mud Weight",
-            uom: "ppg",
-            defaultValue: 0.0
-        }
-    },
-    uom: "ppg",
-    formula: function(args) {
-        return (args.a / 0.052 / args.b) + args.c;
-    },
-    description: "formula: ppg = (Leak-off Pressure, psi) ÷ 0.052 ÷ (Casing Shoe TVD, ft) + (mud weight, ppg)"
-}, {
-    name: "Pump Out (PO) Triplex Pump, bbl/stk",
-    categories: [ "Basic Formulas" ],
-    args: {
-        a: {
-            label: "Liner Diameter",
-            uom: "in",
-            defaultValue: 0
-        },
-        b: {
-            label: "Stroke Length",
-            uom: "in",
-            defaultValue: 0
-        },
-        c: {
-            label: "Efficiency",
-            uom: "%",
-            defaultValue: 100
-        }
-    },
-    uom: "bbl/stk",
-    decimals: 5,
-    formula: function(args) {
-        return 0.000243 * Math.pow(args.a, 2) * args.b * (args.c/100);
-    },
-    description: "formula: bbl/stk = 0.000243 x (liner diameter, in.)&sup2 X (stroke length, in.)"
-}, {
-    name: "Pump Out (PO) Triplex Pump, gpm",
-    categories: [ "Basic Formulas" ],
-    args: {
-        a: {
-            label: "Liner Diameter",
-            uom: "in",
-            defaultValue: 0
-        },
-        b: {
-            label: "Stroke Length",
-            uom: "in",
-            defaultValue: 0
-        },
-        c: {
-            label: "Strokes Per Minute",
-            uom: "SPM",
-            defaultValue: 0
-        }
-    },
-    uom: "gpm",
-    formula: function(args) {
-        return (3 * (Math.pow(args.a, 2) * 0.7854) * args.b) * 0.00411 * args.c;
-    },
-    description: "formula: gpm = [3 x ((liner diameter, in.)&sup2 x 0.7854) x (stroke length, in.)] 0.00411 x (strokes per minute)"
-}, {
-    name: "Pump Out (PO) Duplex Pump, bbl/stk",
-    categories: [ "Basic Formulas" ],
-    args: {
-        a: {
-            label: "Liner Diameter",
-            uom: "in",
-            defaultValue: 0
-        },
-        b: {
-            label: "Stroke Length",
-            uom: "in",
-            defaultValue: 0
-        },
-        c: {
-            label: "Rod Diameter",
-            uom: "in",
-            defaultValue: 0
-        },
-        d: {
-            label: "Efficiency",
-            uom: "%",
-            defaultValue: 100
-        }
-    },
-    uom: "bbl/stk",
-    decimals: 5,
-    formula: function(args) {
-        return 0.000162 * args.b * (2 * Math.pow(args.a, 2) - Math.pow(args.c, 2)) * (args.d/100);
-    },
-    description: "formula: bbl/stk = 0.000162 x (stroke length, in.) x (2 x (liner diameter, in.)&sup2 - (rod diameter, in.)&sup2)"
-}, {
-    name: "Increase Mud Density",
-    categories: [ "Drilling Fluids" ],
-    args: {
-        a: {
-            label: "Starting Density",
-            uom: "ppg",
-            defaultValue: 0.0
-        },
-        b: {
-            label: "Desirable Density",
-            uom: "ppg",
-            defaultValue: 0.0
-        }
-    },
-    uom: "sk/100 bbl",
-    formula: function(args) {
-        return (1470 * (args.b - args.a))/(35 - args.b);
-    }
-}];
+    db.run("Dropping table 'calculators'...", function(r) {
+        return r.tableDrop("calculators");
+    }).then(function() {
+        next();
+    });
+
+};
--- src/migrations/db.js
+++ src/migrations/db.js
@@ -1,112 +1,40 @@
 var r = require("rethinkdb");
 var when = require("when");
 
-exports.connectToDB = function() {
-    return when.promise(function(resolve, reject, notify) {
-        console.log("==> Connecting to database: " + process.env.DB_DATABASE +
-                    " on " + process.env.DB_HOST + ":" + process.env.DB_PORT + "...");
-        r.connect({
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
-            db: process.env.DB_DATABASE
-        }, function(err, conn) {
-            if (err) {
-                console.log("====> Error connecting to DB! Reason: " + err.message);
-                reject(err);
-            } else {
-                console.log("    > Success");
-                resolve({ conn: conn });
-            }
+var conn;
+
+exports.run = function (message, queryCallback) {
+    return when(conn ? conn : connect(), function(conn) {
+        return when.promise(function(resolve, reject, notify) {
+            console.log("==> " + message);
+            queryCallback(r).
+                run(conn, function(err, result) {
+                    if (err) {
+                        console.log("====> DB failed with: " + err.message);
+                        reject(err);
+                    } else {
+                        console.log("    > Success");
+                        resolve(result);
+                    }
+                });
         });
     });
 };
 
-exports.createTable = function(tableName) {
-    return function(obj) {
-        return when.promise(function(resolve, reject, notify) {
-            console.log("==> Creating table: " + tableName + "...");
-            r.tableCreate(tableName).
-                run(obj.conn, function(err, result) {
-                    if (err) {
-                        console.log("====> Error creating table '" + tableName + "'! Reason: " + err.message);
-                        reject(err);
-                    } else {
-                        console.log("    > Success");
-                        resolve({
-                            conn: obj.conn,
-                            table: r.table(tableName),
-                            result: result
-                        });
-                    }
-                });
+var connect = function() {
+    return when.promise(function(resolve, reject, notify) {
+        r.connect({
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            db: process.env.DB_DATABASE
+        }, function(err, connection) {
+            if (err) {
+                console.log("==> Error connecting to DB: " + err.message);
+                reject(err);
+            } else {
+                conn = connection;
+                resolve(connection);
+            }
         });
-    };
-};
-
-exports.createIndex = function(indexName) {
-    return function(obj) {
-        return when.promise(function(resolve, reject, notify) {
-            obj.table.info().run(obj.conn, function(err, result) {
-                var tableName = result.name;
-                console.log("==> Creating index: " + indexName + " on table " + tableName + "...");
-                obj.table.indexCreate(indexName).
-                    run(obj.conn, function(err, result) {
-                        if (err) {
-                            console.log("====> Error creating index '" + indexName + "' on table '" + tableName + "'! Reason: " + err.message);
-                            reject(err);
-                        } else {
-                            console.log("    > Success");
-                            resolve({
-                                conn: obj.conn,
-                                table: obj.table,
-                                result: result
-                            });
-                        }
-                    });
-            });
-        });
-    };
-};
-
-exports.insert = function(data) {
-    return function(obj) {
-        return when.promise(function(resolve, reject, notify) {
-            console.log("==> Inserting data into table...");
-            obj.table.insert(data).
-                run(obj.conn, function(err, result) {
-                    if (err) {
-                        console.log("====> Error inserting data! Reason: " + err.message);
-                        reject(err);
-                    } else {
-                        console.log("    > Success");
-                        resolve({
-                            conn: obj.conn,
-                            table: obj.table,
-                            result: result
-                        });
-                    }
-                });
-        });
-    };
-};
-
-exports.dropTable = function(tableName) {
-    return function(obj) {
-        return when.promise(function(resolve, reject, notify) {
-            console.log("==> Dropping table: " + tableName + "...");
-            r.tableDrop(tableName).
-                run(obj.conn, function(err, result) {
-                    if (err) {
-                        console.log("====> Error dropping table '" + tableName + "'! Reason: " + err.message);
-                        reject(err);
-                    } else {
-                        console.log("    > Success");
-                        resolve({
-                            conn: obj.conn,
-                            result: result
-                        });
-                    }
-                });
-        });
-    };
+    });
 };
